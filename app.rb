753b1847a44e9a81c5d4580777dda329b97b4186
Joseph Knopke <josephknopke@gmail.com>
2021-01-27 13:17:36 -0600

    commit tree c4b2eb32af35234ccc9bd9ff9ef87a1423fcfeb2

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Joseph"
+    end
+
+    get '/hometown' do
+        "My hometown is Kansas City"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Nights"
+    end
+    
 end
